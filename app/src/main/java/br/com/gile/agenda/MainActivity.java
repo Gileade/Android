7f084577b43348e62d6803bb03cd3a2b844393cd
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-09-08 18:51:28 -0300

    Add chamada do layout

--- app/src/main/java/br/com/gile/agenda/MainActivity.java
+++ app/src/main/java/br/com/gile/agenda/MainActivity.java
@@ -2,7 +2,6 @@ package br.com.gile.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -11,8 +10,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Gileade Trindade");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
     }
 }
